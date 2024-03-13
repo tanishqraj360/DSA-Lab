fff27508c223dc9b53cd4844dc1f16efb4ac9818
Tanishq Raj <tanishqraj306@gmail.com>
2024-03-14 00:10:00 +0530

    Done 10b

--- 10b.c
+++ 10b.c
@@ -1,2 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+
+int fib(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (fib(n - 1) + fib(n - 2));
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    int result = fib(n - 1);
+    printf("%d", result);
+}
